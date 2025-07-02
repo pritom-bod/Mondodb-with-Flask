34acc32cfebd2381b6a7095050e194ef33b5112e
pritom-bod <pritomsarker406@gmail.com>
2025-07-02 15:55:57 +0600

    fix urls home page

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from routes.author_routes import author_bp
 from routes.post_routes import post_bp
 
 app = Flask(__name__)
 
-# register routes
 app.register_blueprint(author_bp)
 app.register_blueprint(post_bp)
+
+# root url redirect to authors
+@app.route("/")
+def home():
+    return redirect(url_for("author.list_authors"))
 
 if __name__ == "__main__":
     app.run(debug=True)
